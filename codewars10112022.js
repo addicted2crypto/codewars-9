1df17d82a37619df8723509cc14b42d6e9cdf1e3
Willim <tundra2727@gmail.com>
2022-10-11 12:05:30 -0700

    added my daily codewars on lunch today

---
+++ codewars10112022.js
@@ -0,0 +1,16 @@
+// Title: Volume of cuboid.
+
+// Description: Bob needs a fast way to calculate the volume of a cuboid 
+// with three values: the length, width and height of the cuboid. Write a
+//  function to help Bob with this calculation.
+
+// GEOMETRYFUNDAMENTALS
+
+// My soluttion =>
+addicted2krypto
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+     let volumeCaculated = (length * height* width)
+    return volumeCaculated 
+ }
+}
