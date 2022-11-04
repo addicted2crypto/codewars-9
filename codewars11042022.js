95e34b8bab1bdb323bcc37d0b5cef1c04b6b810f
Willim <tundra2727@gmail.com>
2022-11-04 11:11:09 -0700

    daily codewars 11/04/2022

---
+++ codewars11042022.js
@@ -0,0 +1,14 @@
+// Title: MakeUpperCase. 
+
+// DESCRIPTION:
+// Write a function which converts the input string to uppercase.
+
+// FUNDAMENTALSSTRINGS
+
+// My solution =>
+
+let makeUpperCase = (str) => str.toUpperCase();
+  
+// function makeUpperCase(str){
+//   return str.toUpperCase();
+// }
