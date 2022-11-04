64a32ddf2715fc63622ca57faa710473dc05fa61
Willim <tundra2727@gmail.com>
2022-11-03 17:23:44 -0700

    daily codewars 11/3/2022

---
+++ cocewars11022022.js
@@ -0,0 +1,18 @@
+// Title: Area of a square.
+
+// DESCRIPTION:
+// Complete the function that calculates the area of the red square,
+//  when the length of the circular arc A is given as the input. Return 
+//  the result rounded to two decimals.
+
+// Graph
+
+// Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
+
+
+// My solution =>
+
+// function squareArea(A){
+//   return (2 * A / Math.PI)** 2;
+// }
+squareArea = (A) => (2 * A / Math.PI) ** 2;
